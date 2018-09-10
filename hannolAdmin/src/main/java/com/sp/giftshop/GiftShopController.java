d4b8cbe33533f8ba6f887828e4baf148614dfe3e
sist <sist@sist-182>
2018-09-10 18:38:52 +0900

    smartEditer js 추가

---
+++ hannolAdmin/src/main/java/com/sp/giftshop/GiftShopController.java
@@ -0,0 +1,17 @@
+package com.sp.giftshop;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller("giftshop.giftShopController")
+public class GiftShopController {
+	
+	@RequestMapping(value="/giftshop/created", method=RequestMethod.GET)
+	public String createdForm(Model model) throws Exception{
+		model.addAttribute("mode", "created");
+		return ".giftshop.created";
+	}
+
+}
