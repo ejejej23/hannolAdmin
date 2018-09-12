c68bbd95f743ff5d8928a73321f081827ad4d661
user <user@user-pc>
2018-09-12 12:12:34 +0900

    기본 body폰트 14px -> 12px 변경 부트스트랩이랑 겹치는 btn -> btn1으로 변경

--- hannolAdmin/src/main/webapp/resource/js/common.js
+++ hannolAdmin/src/main/webapp/resource/js/common.js
@@ -1,10 +1,10 @@
 //메뉴 슬라이드 업다운
 $(function(){
 	$(".menu_icon").click(function(){
-		if(!$(".nav").is(":visible")){
-			$(".nav").slideDown();
+		if(!$(".navMenu").is(":visible")){
+			$(".navMenu").slideDown();
 		}else{
-			$(".nav").slideUp();
+			$(".navMenu").slideUp();
 		}
 	});
 });
