b129caf3d5524e66931950f57fe29bd09dd5ab8c
user <user@user-pc>
2018-10-04 18:26:56 +0900

    jwyang 컴터 느려ㅠㅠ

---
+++ hannolAdmin/src/main/java/com/sp/grade/Grade.java
@@ -0,0 +1,59 @@
+package com.sp.grade;
+
+public class Grade {
+	private int gradeCode;
+	private String gradeName;
+	private String condition;
+	private String benefit;
+	private String originalFileName;
+	private String saveFileName;
+
+	public int getGradeCode() {
+		return gradeCode;
+	}
+
+	public void setGradeCode(int gradeCode) {
+		this.gradeCode = gradeCode;
+	}
+
+	public String getGradeName() {
+		return gradeName;
+	}
+
+	public void setGradeName(String gradeName) {
+		this.gradeName = gradeName;
+	}
+
+	public String getCondition() {
+		return condition;
+	}
+
+	public void setCondition(String condition) {
+		this.condition = condition;
+	}
+
+	public String getBenefit() {
+		return benefit;
+	}
+
+	public void setBenefit(String benefit) {
+		this.benefit = benefit;
+	}
+
+	public String getOriginalFileName() {
+		return originalFileName;
+	}
+
+	public void setOriginalFileName(String originalFileName) {
+		this.originalFileName = originalFileName;
+	}
+
+	public String getSaveFileName() {
+		return saveFileName;
+	}
+
+	public void setSaveFileName(String saveFileName) {
+		this.saveFileName = saveFileName;
+	}
+
+}
