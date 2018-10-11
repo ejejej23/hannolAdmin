97baa9470a0d0e50f3b95b66936ebb849c7ad895
yujin <yujin@DESKTOP-97LK3CK>
2018-10-11 18:13:17 +0900

    공연 예약 header 및 탭 수정

--- hannolAdmin/src/main/java/com/sp/reservation/ReservationController.java
+++ hannolAdmin/src/main/java/com/sp/reservation/ReservationController.java
@@ -22,23 +22,23 @@ public class ReservationController {
 	@Autowired
 	private ReservationService service;
 	
-	@RequestMapping(value="/reservation/list")
+/*	@RequestMapping(value="/reservation/list")
 	public String reservationList(Model model) {
 		model.addAttribute("tab", "show");
 		model.addAttribute("pageNo", "1");
 		return ".reservation.list";
 	}
-
+*/
+	
 	// 공연
 	@RequestMapping(value="/reservation/show/list")
 	public String reservationShowList(
-			@RequestParam(value="pageNo", defaultValue="1") int page,
 			Model model) throws Exception {
 		
 		List<Show> list = service.listNowPlaying();
 		model.addAttribute("showList", list);
 		
-		return "/reservation/showBookList";
+		return ".reservation.showBookList";
 	}
 	
 	// 공연 - selectBox
